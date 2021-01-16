f0edd9774a72b97077eb654a7d053c7e5730e633
Isaac Kojo Yeboah <yeboah.determined.isaac>
2021-01-16 18:57:27 +0000

    update

--- js/app.js
+++ js/app.js
@@ -14,4 +14,24 @@ todoButton.addEventListener('click', addTodo);
 function addTodo(event){
     event.preventDefault();
     console.log("hellow to do lis")
+
+    const todoDiv = document.createElement("div");
+    todoDiv.classList.add("todo");
+     const newTodo = document.createElement("todo");
+    todoDiv.classList.add("todo");
+
+     const newTodo = document.createElement('li')
+     newTodo.innerText = "hey";
+     newTodo.classList.add('todo-item');
+
+     const completedButton = document.createElement('button');
+     completedButton.innerHTML = '<i class="fas fa-check"></i>';
+     completedButton.classList.add("completed-btn");
+     todoDiv.appendChild(completedButton);
+
+     const trashButton = document.createElement('button');
+     completedButton.innerHTML = '<i class="fas fa-trash"></i>';
+     completedButton.classList.add("completed-btn");
+     todoDiv.appendChild(trashButton);
+
 }
